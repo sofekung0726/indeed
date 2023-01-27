95913fcdb062bf08361e1ddad1125e27fbf97315
sofekung0726 <sofekung@gmail.com>
2023-01-27 11:43:44 +0700

    Initial commit

---
+++ Test Suites/TC 1001.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC 1001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b6342fdb-3d9a-4629-959a-5a47ff5f33e7</testSuiteGuid>
+   <testCaseLink>
+      <guid>baa6da00-6c99-4c66-a7f3-2ff3c5270770</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC 1003 Search Excaption</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
